7689282667b3feb1e76425bb162a8485c9577ce5
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-04-12 13:37:33 +0100

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,7 @@
 </script>
 
 <template>
+  <div class="w-full h-48 bg-slate-900">div.bg-slate-900.h-48.w-full</div>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld class="bg-slate-900" msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
-
-<style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
