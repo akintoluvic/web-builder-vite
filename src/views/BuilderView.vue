5c52ddc94cac04a43cb0051d299c52deefbded55
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-04-13 13:52:46 +0100

    fix i

--- src/views/BuilderView.vue
+++ src/views/BuilderView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="h-screen w-screen lg:overflow-x-hidden" :class="[theme, darkMode ? 'dark dark-mode' : 'white']">
+  <div
+    class="h-screen w-screen lg:overflow-x-hidden"
+    :class="[theme, darkMode ? 'dark dark-mode' : 'white']"
+  >
     <!-- Top Bar -->
     <TopBar />
 
-    
     <div class="h-screen flex">
       <!-- components sidebar -->
       <LeftSideBar />
@@ -18,24 +20,24 @@
   </div>
 </template>
 
-<script >
-import TopBar from "@/components/builder/TopBar.vue"
-import LeftSideBar from "@/components/builder/LeftSideBar.vue"
-import { useTheme } from "@/compossable/theme"
-import { useDarkMode } from "@/compossable/dark-mode"
-import CodeOrPagePreview from '@/components/builder/CodeOrPagePreview.vue'
+<script>
+  import TopBar from '@/components/builder/TopBar.vue'
+  import LeftSideBar from '@/components/builder/LeftSideBar.vue'
+  import { useTheme } from '@/compossable/theme'
+  import { useDarkMode } from '@/compossable/dark-mode'
+  import CodeOrPagePreview from '@/components/builder/CodeOrPagePreview.vue'
 
-export default {
-  name: "BuilderView",
-  components: {
-    TopBar,
-    LeftSideBar,
-    CodeOrPagePreview
-  },
-  setup() {
-    const { theme } = useTheme()
-    const { darkMode } = useDarkMode()
-    return { theme, darkMode }
-  },
-}
+  export default {
+    name: 'BuilderView',
+    components: {
+      TopBar,
+      LeftSideBar,
+      CodeOrPagePreview,
+    },
+    setup() {
+      const { theme } = useTheme()
+      const { darkMode } = useDarkMode()
+      return { theme, darkMode }
+    },
+  }
 </script>
