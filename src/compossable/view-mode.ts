e8fd0218d6822f01f17e31faa6ead1782e637d87
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-04-15 14:52:20 +0100

    changed view-mode to ts

---
+++ src/compossable/view-mode.ts
@@ -0,0 +1,51 @@
+import { ref, computed } from 'vue'
+const viewType = ref('desktop')
+const codeView = ref(false)
+
+export function useViewOrCode() {
+  const iframeViewWidth = computed(() => {
+    switch (viewType.value) {
+      case 'desktop':
+        return 'w-full'
+      case 'tablet':
+        return 'max-w-3xl w-full'
+      case 'mobile':
+        return 'max-w-md w-full'
+
+      default:
+        return `w-full`
+    }
+  })
+
+  const deviceViewWidth = computed(() => {
+    switch (viewType.value) {
+      case 'desktop':
+        return 'w-full'
+      case 'tablet':
+        return 'max-w-4xl w-full'
+      case 'mobile':
+        return 'max-w-lg w-full'
+
+      default:
+        return `w-full`
+    }
+  })
+
+  const setViewType = (value: string) => {
+    viewType.value = value
+    codeView.value = false
+  }
+
+  const toggleCodeView = () => {
+    codeView.value = !codeView.value
+  }
+
+  return {
+    setViewType,
+    toggleCodeView,
+    codeView,
+    viewType,
+    iframeViewWidth,
+    deviceViewWidth,
+  }
+}
