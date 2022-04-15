d6766abaea9976c529e8e01415df020ed81694f1
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-04-15 12:07:04 +0100

    fix components turned ref

--- src/compossable/components.js
+++ src/compossable/components.js
@@ -1,39 +1,38 @@
-import { ref, onMounted } from 'vue'
-import getIcons from "@/components/icons"
-import getBlocks from "@/components/blocks"
+import { ref, onMounted, shallowRef } from 'vue'
+import getIcons from '@/components/icons'
+import getBlocks from '@/components/blocks'
 
-const iconList = ref(null)
-const blocksList = ref(null)
-const blockListArr = ref([])
+const iconList = shallowRef(null)
+const blocksList = shallowRef(null)
+const blockListArr = shallowRef([])
 const selectedIcons = ref([])
 
 blocksList.value = getBlocks()
 iconList.value = getIcons()
 
 export function useComponents() {
+  const addComponent = (val) => {
+    selectedIcons.value = [...selectedIcons.value, val]
+  }
 
-    const addComponent = val => {
-      selectedIcons.value = [...selectedIcons.value, val]
-    }
+  const removeComponent = (val) => {
+    selectedIcons.value.splice(val, 1)
+  }
 
-    const removeComponent = val => {
-      selectedIcons.value.splice(val, 1)
-    }
+  onMounted(() => {
+    Object.entries(iconList).forEach(([type, icons]) => {
+      Object.keys(icons).map((name) =>
+        blockListArr.value.push(`${name},${type}`)
+      )
+    })
+  })
 
-    onMounted(() => {
-        Object.entries(iconList).forEach(([type, icons]) => {
-          Object.keys(icons).map((name) =>
-            blockListArr.value.push(`${name},${type}`)
-          );
-        })
-      })
-
-    return {
-      iconList,
-      blocksList,
-      blockListArr,
-      selectedIcons,
-      addComponent,
-      removeComponent
-    }
+  return {
+    iconList,
+    blocksList,
+    blockListArr,
+    selectedIcons,
+    addComponent,
+    removeComponent,
+  }
 }
