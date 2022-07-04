cf4843fb846dc9ebf4615be7d8e87390ef96424c
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-07-04 12:08:02 +0100

    props init

---
+++ src/compossable/props.ts
@@ -0,0 +1,7 @@
+export const propsObject = {
+  HeroB: { heroType: 'center' },
+  HeroC: { heroType: 'image-first' },
+  HeaderB: { navOption: 'all-left' },
+  HeaderC: { navOption: 'brand-middle' },
+  HeaderD: { navOption: 'space-between' },
+}
